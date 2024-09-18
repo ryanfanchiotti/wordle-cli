80833c759af4557081bc06fc05bbcc397c31e5fd
ryanfanchiotti <ryan@rur.com>
2024-09-17 23:29:28 -0400

    edit comments

--- src/analysis.rs
+++ src/analysis.rs
@@ -1,14 +1,3 @@
-/* 
-for each guess:
-    check how many other words it would remove if the answer was each possible answer
-    create table of removals for each letter in each space
-    average the amount of removals
-display:
-    rank possible words
-    show best possible word
-multithreading (?)
-*/
-
 use std::cmp::min;
 
 // check if a word is possible after a guess (assuming the correct answer)
